ad83a5ea986fbb2086370ee13bf28a737158cc2c
ttreit <14933277+ttreit@users.noreply.github.com>
2024-09-29 20:41:20 -0700

    add nephew is awesome route

--- backend/index.ts
+++ backend/index.ts
@@ -8,4 +8,11 @@ app.get("/", (request: Request, response: Response)=>{
     response.send("I am using typescript - HELLO WORLD - Mr. PotatoHead MR POTATOHEAD")
 })
 
+app.get("/nephew-is-awesome", (request: Request, response: Response)=>{
+  response.json({nephew: "Andreas",
+                isAwesome: true,
+                isSuperAwesome: true,
+  })
+})
+
 app.listen(port, ()=>console.log(`App lisening on ${port}`) );
