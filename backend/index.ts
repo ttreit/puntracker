25a7514bca4a7bcf06462d0cbf61f59561dcceb2
ttreit <14933277+ttreit@users.noreply.github.com>
2024-09-30 21:28:09 -0700

    remove Andreas' silliness

--- backend/index.ts
+++ backend/index.ts
@@ -4,15 +4,8 @@ import {Request, Response} from "express";
 const app = express();
 const port:number = 7171;
 
-app.get("/", (request: Request, response: Response)=>{
-    response.send("I am using typescript - HELLO WORLD - Mr. PotatoHead MR POTATOHEAD")
+app.get("/", (request: Request, response: Response) => {
+    response.send("I am using typescript - Hi Kraken - <b>Mr. PotatoHead MR POTATOHEAD</b>");
 })
 
-app.get("/nephew-is-awesome", (request: Request, response: Response)=>{
-  response.json({nephew: "Andreas",
-                isAwesome: true,
-                isSuperAwesome: true,
-  })
-})
-
-app.listen(port, ()=>console.log(`App lisening on ${port}`) );
+app.listen(port, () => console.log(`App lisening on ${port}`));
