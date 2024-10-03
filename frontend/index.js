fb543a5fc2b58325c578b8aa1d2a96114c1ce1ff
ttreit <14933277+ttreit@users.noreply.github.com>
2024-10-02 22:50:50 -0700

    create add pun button

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,8 @@
-alert("Now I'm working from a JS file - install TS you lazy dawgs");
+//access button in JS (Dom element)
+const addPunButton = document.getElementById('add-pun-button');
+console.log('Test AddPunButton: ', addPunButton);
+
+//add click listener
+addPunButton.addEventListener('click', () => {
+    alert('Testing Click Listener for Add Pun button');
+})
