fe6c27b0d7e1692f947e92fc6affe089d288fc0f
Andreas <minibumhead281@gmail.com>
2024-10-05 02:13:52 +0200

    add frontend buttons and click envent listeners

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,36 @@
 //access button in JS (Dom element)
 const addPunButton = document.getElementById('add-pun-button');
-console.log('Test AddPunButton: ', addPunButton);
+const fetchPunsButton = document.getElementById('fetch-puns-button');
+const punList = document.getElementById('pun-list');
+const baseApiUrl = 'http://localhost:7171'
+const defaultHeaders = {
+    'Content-Type': 'application/json',
+}
 
-//add click listener
-addPunButton.addEventListener('click', () => {
-    alert('Testing Click Listener for Add Pun button');
+//add click listener for post request
+addPunButton.addEventListener('click', async () => {
+    try {
+        const res = await fetch(`${baseApiUrl}/puns`, {
+            headers: { ...defaultHeaders },
+            method: 'POST',
+            body: JSON.stringify({ pun: 'I can not think of a pun' })
+        })
+        console.log(res)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+//add click listener for get request
+fetchPunsButton.addEventListener('click', async () => {
+    try {
+        const res = await fetch(`${baseApiUrl}/puns`)
+        const data = await res.json()
+        for (let pun of data) {
+            punList.innerHTML += `<p>${pun.pun}</p>`
+        }
+    }
+    catch (error) {
+        console.error(error)
+    }
 })
